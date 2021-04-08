e2b8a4a2d20e42444263182a0891b5ebf5847027
Joberth David Borba Neves <joberthdavid@hotmail.com>
2021-04-07 22:23:05 -0300

    Refatoracao de classes de extracao de equipamentos

--- pdf_to_txt_sintetico_equipamento.py
+++ pdf_to_txt_sintetico_equipamento.py
@@ -4,24 +4,6 @@ from datetime import datetime
 from progress.bar import Bar
 from progress.bar import PixelBar
 
-from funcoes import (
-                        iniciar_dicionario_arquivo,
-                        retornar_regex_arquivo,
-                        configurar_dicionario_arquivo,
-                        retornar_item,
-                        retornar_path,
-                        retornar_origem,
-                        gerar_arquivo_dados_basicos,
-                        gerar_arquivo_custos_unitarios_insumos,
-                        gerar_arquivo_detalhamento_custos_equipamento,
-                        iniciar_dicionario,
-                        retornar_regex,
-                        retornar_regex_cabecalho,
-                        configurar_dicionario,
-                        escrever_arquivo_cadastro,
-                        escrever_arquivo_custos,
-                        escrever_arquivo_detalhamento_custos
-                    )
 from classes import (
                         RegexEquipamento,
                         Equipamento,
@@ -44,28 +26,8 @@ pdf_file_desonerado = "SICRO/GO 10-2020 Relatório Sintético de Equipamentos - 
 with open( pdf_file_desonerado, "rb" ) as f:
     cadastro_desonerado = pdftotext.PDF( f )
 
-##### Extraindo dados dos PDF
 
-d_arquivo = iniciar_dicionario_arquivo()
-regex_arquivo = retornar_regex_arquivo( pdf_file_onerado )
-
-if ( regex_arquivo is not None ):
-    configurar_dicionario_arquivo( d_arquivo, regex_arquivo )
-
-item = retornar_item( d_arquivo )
-
-path =  retornar_path( d_arquivo )
-
-origem_dados = retornar_origem( d_arquivo )
-
-arquivo_equipamento = gerar_arquivo_dados_basicos( path )
-
-custos_unitarios = gerar_arquivo_custos_unitarios_insumos( path )
-
-detalhamento_custos = gerar_arquivo_detalhamento_custos_equipamento( path )
-
-
-with PixelBar('Extraindo dados do PDF', max=num_pages, suffix='%(index)d/%(max)d - %(percent).1f%% - %(eta)ds') as bar:
+with PixelBar('Extraindo dados do PDF', max=2*num_pages, suffix='%(index)d/%(max)d - %(percent).1f%% - %(eta)ds') as bar:
 
 ###### Populando lista com instância de Equipamento
 
@@ -84,6 +46,7 @@ with PixelBar('Extraindo dados do PDF', max=num_pages, suffix='%(index)d/%(max)d
                 obj_equipamento = Equipamento( obj_regex_onerado.principal )
                 lista_equipamento.append( obj_equipamento )
 
+        bar.next()
 
 ###### fazendo o mesmo para o arquivo desonerado
 
@@ -102,6 +65,8 @@ with PixelBar('Extraindo dados do PDF', max=num_pages, suffix='%(index)d/%(max)d
                 obj_equipamento = Equipamento( obj_regex_desonerado.principal )
                 lista_equipamento_auxiliar.append( obj_equipamento )  
 
+        bar.next()
+
 ###### compilando os dados na lista_equipamento
 
     for item_onerado in lista_equipamento:
@@ -111,8 +76,6 @@ with PixelBar('Extraindo dados do PDF', max=num_pages, suffix='%(index)d/%(max)d
                 item_onerado.custo_improdutivo_desonerado = item_desonerado.custo_improdutivo_onerado
                 item_onerado.mao_de_obra_desonerado = item_desonerado.mao_de_obra_onerado
                 
-        bar.next()
-
 
 ##### Escrevendo arquivo TXT
 
